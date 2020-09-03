1c1c8681c6fef3a5b21eb7f775dd02e16972208d
vimiomori <vivian.muchen@gmail.com>
2020-09-03 18:07:04 +0900

    implement all CRUD operations

--- main.py
+++ main.py
@@ -1,81 +1,91 @@
 import logging
+from bson import ObjectId
 from datetime import datetime
-from flask import render_template, Flask, abort, request
+from flask import render_template, Flask, abort, request, redirect, url_for
 from flask_api import FlaskAPI
+from pymongo import MongoClient
 
 app = FlaskAPI(__name__)
 logging.getLogger().setLevel(logging.DEBUG)
 
 @app.route('/')
 def home():
-    abort(500)
-    test = {
-        "msg": "test",
-        "status": False
-    }
-    message = "ToDo List"
-    return render_template('index.html', message=message)
+    return redirect(url_for('tasks'))
 
 
-@app.route('/new_page')
-def new_page():
-    message = "new page"
-    return render_template('new_page.html', message=message)
-
-
-@app.route('/time')
-def time():
-    now = datetime.now()
-    message = now.strftime("$Y/$m/$d %H:%M:%S")
-    return render_template('new_page.html', message=message)
-
-
-@app.route('/template_form', methods=['GET', 'POST'])
-def template_form():
-    if request.method == 'GET':
-        return render_template('template_form.html')
-    elif request.method == 'POST':
-        return request.form['message']
-
-
-@app.route('/api/tasks/<task_id>')
-@app.route('/api/tasks', methods=['GET', 'POST'])
-def _tasks(task_id=None):
-    if request.method == 'GET':
-        if task_id:
-            res = {
-                'id': task_id,
-                'content': 'task',
-                'done': False,
-            }
-            return res, 200
-        else:
-            task1 = {
-                'id': 1,
-                'content': 'first task',
-                'done': True,
-            }
-            task2 = {
-                'id': 2,
-                'content': 'second task',
-                'done': False,
-            }
-            task3 = {
-                'id': 3,
-                'content': 'third task',
-                'done': False,
-            }
-            res = dict(task1=task1, task2=task2, task3=task3)
-            return res
-    elif request.method == 'POST':
-        # json_data = request.get_json()
-        content = request.data['content']
-        res = {
-            'id': 999,
-            'content': content,
-            'done': False,
+def insert(data):
+    with MongoClient("127.0.0.1", 27017) as client:
+        db = client.todo_app
+        tasks_collection = db.tasks_collection
+        task = {
+            "content": data['content'],
+            "do_by": datetime.strptime(data['do_by'], "%m/%d/%Y"),
+            "done": data['done']
         }
-        return res, 201
+        result = tasks_collection.insert_one(task)
+        task['_id'] = str(result.inserted_id)
+        return task
+
+
+def find():
+    with MongoClient("127.0.0.1", 27017) as client:
+        db = client.todo_app
+        tasks_collection = db.tasks_collection
+        tasks = []
+        for task in tasks_collection.find():
+            task['_id'] = str(task['_id'])
+            tasks.append(task)
+        return tasks
+
+
+def find_by(_id):
+    with MongoClient("127.0.0.1", 27017) as client:
+        db = client.todo_app
+        tasks_collection = db.tasks_collection
+        task = tasks_collection.find_one({'_id': ObjectId(_id)})
+        task['_id'] = str(task['_id'])
+        return task
+
+
+def update(task):
+    with MongoClient("127.0.0.1", 27017) as client:
+        db = client.todo_app
+        tasks_collection = db.tasks_collection
+        filter = {'_id': ObjectId(task['_id'])}
+        update = {
+            '$set': dict(list(task.items())[1:])
+        }
+        tasks_collection.update_one(filter, update)
+        return task
+
+
+def delete(_id):
+    with MongoClient("127.0.0.1", 27017) as client:
+        db = client.todo_app
+        tasks_collection = db.tasks_collection
+        filter = {'_id': ObjectId(str(_id))}
+        result = tasks_collection.delete_one(filter)
+        return result.deleted_count
+
+
+@app.route('/api/tasks/<task_id>', methods=['GET', 'DELETE'])
+def task_details(task_id):
+    if request.method == 'GET':
+        return find_by(task_id), 200
+    elif request.method == 'DELETE':
+        deleted_count = delete(task_id)
+        if deleted_count != 1:
+            return f"Error: Failed to delete task (id: {task_id})", 400
+        return '', 204
+
+@app.route('/api/tasks', methods=['GET', 'POST', 'PUT'])
+def tasks():
+    if request.method == 'GET':
+        return {'tasks': find()}, 200
+    elif request.method == 'POST':
+        return insert(request.data), 201
+    elif request.method == 'PUT':
+        return update(request.data), 200
 
 
 @app.errorhandler(404)
